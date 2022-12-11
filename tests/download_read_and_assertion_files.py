ab0fa555b0a8964a76f51d137da63c208e6f2f2e
Nikita Fedotov <nik197.1997@gmail.com>
2022-12-11 18:09:29 +0300

    added new pdf, xlsx, csv files in project

--- tests/download_read_and_assertion_files.py
+++ tests/download_read_and_assertion_files.py
@@ -5,14 +5,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import os
 
-# url = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
-# response = requests.get(url=url)
-# content = response.content
+# Создание файла pdf
+resourses_pdf_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..', r'resourses\pdf_test.pdf')))
 
-resourses_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'resourses\pdf_test.pdf')))
+pdf_url = 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_PDF.pdf'
+response = requests.get(pdf_url, stream = True)
 
-url = 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_PDF.pdf'
-r = requests.get(url, stream = True)
+with open(resourses_pdf_dir, 'wb') as pdf_file:
+    pdf_file.write(response.content)
 
-with open(resourses_dir, 'wb') as pdf_file:
-    pdf_file.write(r.content)
+# Создание файла xlsx
+resourses_xlsx_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..', r'resourses\xlsx_test.xlsx')))
+
+xlsx_url = 'https://download.samplelib.com/xls/sample-simple-1.xls'
+response = requests.get(xlsx_url, stream = True)
+
+with open(resourses_xlsx_dir, 'wb') as xlsx_file:
+    xlsx_file.write(response.content)
+
+# Создание файла csv
+resourses_csv_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..', r'resourses\csv_test.csv')))
+
+csv_url = 'https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv'
+response = requests.get(csv_url, stream = True)
+
+with open(resourses_csv_dir, 'wb') as csv_file:
+    csv_file.write(response.content)
+
+#Добавление файлов в архив
+#tbd
